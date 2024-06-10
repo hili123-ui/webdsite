139c4b3d746b105f078457b29657799d91850257
hili123-ui <lhi236185@gmail.com>
2024-06-10 22:05:33 +0300

    Update app1.js

--- apps/app1.js
+++ apps/app1.js
@@ -54,7 +54,7 @@ function initApp(){
         let newDiv = document.createElement('div');
         newDiv.classList.add('item');
         newDiv.innerHTML = `
-            <img src="svg./${value.image}">
+            <img src="svg/${value.image}">
             <div class="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>
             <button onclick="addToCard(${key})">اضافه الي العربه</button>`;
@@ -80,7 +80,7 @@ function reloadCard(){
         if(value != null){
             let newDiv = document.createElement('li');
             newDiv.innerHTML = `
-                <div><img src="./svg/${value.image}"/></div>
+                <div><img src="svg/${value.image}"/></div>
                 <div>${value.name}</div>
                 <div>${value.price.toLocaleString()}</div>
                 <div>
